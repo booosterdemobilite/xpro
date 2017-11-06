a8cb0c8dd61465153bc812ed4b3de89bc0a615c2
soulmakh <soulmakh@users.noreply.github.com>
2017-11-06 02:18:16 +0100

    nouveau template

---
+++ template-parcours-bma.php
@@ -0,0 +1,38 @@
+<?php
+// =============================================================================
+// TEMPLATE NAME: BMA - Parcours
+// -----------------------------------------------------------------------------
+// 
+// =============================================================================
+
+?>
+<!DOCTYPE html>
+<!--[if lte IE 7]> <html class="ie-lte7" dir="ltr"> <![endif]-->
+<!--[if lte IE 9]> <html class="ie-lte9" dir="ltr"> <![endif]-->
+<!--[if !IE]><!--> <html dir="ltr">             <!--<![endif]-->
+<html>
+
+<head>
+	<?php  
+		include SO_SEQUENCE_MANAGER__PLUGIN_DIR . "includes/themes/header/header-parcours.php";
+	?>
+</head>
+<body ng-app="exerciceApp">
+
+	<?php while ( have_posts() ) : the_post(); ?>
+		
+		<?php 
+		 	$query = get_post(get_the_ID()); 
+    		$content = apply_filters('the_content', $query->post_content);
+
+    		echo $content;
+    	?>
+
+    <?php endwhile; ?>
+
+    
+    <?php  
+		include SO_SEQUENCE_MANAGER__PLUGIN_DIR . "includes/themes/footer/footer-parcours.php";
+	?>
+</body>
+</html>
